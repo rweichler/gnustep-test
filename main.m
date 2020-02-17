5f37c8fcc61723c4cab698475ab5072c8947b9eb
Reed Weichler <rweichler@gmail.com>
2020-02-17 16:25:41 -0500

    add commented-out crappy code for adding button that doesnt work

--- main.m
+++ main.m
@@ -18,6 +18,21 @@
 -(void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     printf("app did finish launching\n");
+    self.window = [NSWindow.alloc init];
+
+    NSAlert *alert = [[NSAlert.alloc init] autorelease];
+    alert.messageText = @"Goodbye, world!";
+    alert.informativeText = @"BYEEEE";
+
+    /*
+    NSButton *button = [alert addButtonWithTitle:@"Cya"];
+    [button retain];
+    button.frame = NSMakeRect(0, 0, 50, 50);
+    printf("%s %s\n", button.description.UTF8String, button.superview.description.UTF8String);
+    printf("%s\n", alert.description.UTF8String);
+    */
+
+    [alert runModal];
 }
 
 @end
